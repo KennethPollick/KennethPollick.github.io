069f8117c1d742a1a5313c1fdd577161e2c42fe4
Kenneth Pollick <kennethpollick@gmail.com>
2017-07-30 17:33:01 -0400

    pages set up with some content

--- contact_form.php
+++ contact_form.php
@@ -2,39 +2,32 @@
 
 /*********************************************
  *AUTHOR:   Kenneth Pollick
- *DATE:     24-4-2017
- *PURPOSE:  To send an email to Giering Media based on the information from the Contact page
+ *DATE:     2017-7-30
+ *PURPOSE:  To send an email to Kenneth Pollick based on the information from the Contact page of Kenneth Pollick Software
 **********************************************/
 
 //definition of who to send the email to
-define("SEND_TO", "colin.giering@gmail.com");
+define("SEND_TO", "kennethpollick@gmail.com");
 
 //scrubbing of inputs
 $name = trim( stripslashes( htmlspecialchars( $_POST["name"] ) ) );
-$phonenumber = trim( stripslashes( htmlspecialchars( $_POST["phonenumber"] ) ) );
 $email = trim( stripslashes( htmlspecialchars( $_POST["email"] ) ) );
 $message = wordwrap( trim( stripslashes( htmlspecialchars( $_POST["message"] ) ) ), 70);//wordwrap is needed at more than 70 characters for the message argument of the mail function
-$subject = "";
+$subject = ("KennethPollickSoftwareForm[".$name.", ".$email."]");
 
-if (strlen($phonenumber) > 0) {
-    $subject = ("GieringMediaForm[".$name.", ".$email.", ".$phonenumber."]");
-}
-else {
-    $subject = ("GieringMediaForm[".$name.", ".$email."]");
-}
 
 
 //the actual mailing
 mail(SEND_TO, $subject, $message);
 
 
-//redirect to the services page
-header('Location: thanks.html');
+//redirect to the index
+header('Location: index.html');
 ?>
 
 <!-- alert the user that their message was sent -->
 <script type="text/javascript">
-alert("Message Sent");
+alert("Message Sent.\nThank you for your Feedback.");
 </script>
 
 <?php
